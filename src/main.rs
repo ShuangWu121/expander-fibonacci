4bc1d31927b9d33d505b23592c050ff55795d776
ShuangWu121 <shuang.wu@posteo.net>
2024-08-13 00:07:59 +0200

    build fibonacci circuit with custome number of layers and copies, use gkr to prove and verify
    Signed-off-by: ShuangWu121 <shuang.wu@posteo.net>

--- src/main.rs
+++ src/main.rs
@@ -9,130 +9,53 @@ use sha2::Digest;
 use rand::Rng;
 
 
-fn gen_simple_circuit<C: GKRConfig>() -> Circuit<C> {
-    let mut circuit = Circuit::default();
-    let mut l0 = CircuitLayer::default();
-    l0.input_var_num = 2;
-    l0.output_var_num = 2;
-    l0.add.push(GateAdd {
-        i_ids: [0],
-        o_id: 0,
-        coef: C::CircuitField::from(1),
-        is_random: false,
-        gate_type: 1,
-    });
-
-    l0.add.push(GateAdd {
-        i_ids: [0],
-        o_id: 0,
-        coef: C::CircuitField::from(1),
-        is_random: false,
-        gate_type: 1,
-    });
-
-
-    l0.add.push(GateAdd {
-        i_ids: [0],
-        o_id: 1,
-        coef: C::CircuitField::from(1),
-        is_random: false,
-        gate_type: 1,
-    });
-    l0.add.push(GateAdd {
-        i_ids: [1],
-        o_id: 1,
-        coef: C::CircuitField::from(1),
-        is_random: false,
-        gate_type: 1,
-    });
-    l0.mul.push(GateMul {
-        i_ids: [0, 2],
-        o_id: 2,
-        coef: C::CircuitField::from(1),
-        is_random: false,
-        gate_type: 1,
-    });
-    circuit.layers.push(l0.clone());
-    circuit
-}
-
-fn gen_one_layer_fibonacci_circuit<C: GKRConfig>() -> Circuit<BN254Config> {
+//build fibonacci circuit with layernumber layers and copynumber of copies
+fn build_fibonacci(layernumber: usize,copynumber: usize)->Circuit<BN254Config>{
     let mut circuit=Circuit::default();
-    let mut l0=CircuitLayer::default();
-
-    l0.input_var_num=1;
-    l0.output_var_num=1;
-
-    l0.add.push(GateAdd{
-        i_ids:[0],
-        o_id:1,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
-    });
-
-    l0.add.push(GateAdd{
-        i_ids:[1],
-        o_id:1,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
-    });
-
-    l0.add.push(GateAdd{
-        i_ids:[1],
-        o_id:0,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
-    });
-
-
-
-    circuit.layers.push(l0.clone());
+    let mut l0=CircuitLayer::<BN254Config>::default();
     
-    let mut l1=CircuitLayer::<BN254Config>::default();
-    l1.input_var_num=1;
-    l1.output_var_num=1;
+    l0.input_var_num=((2 * copynumber) as f64).log2().ceil() as usize;
+    println!("input_var_num {:?}",l0.input_var_num);
+    l0.output_var_num=((2 * copynumber) as f64).log2().ceil() as usize;
 
-    l1.add.push(GateAdd{
-        i_ids:[0],
-        o_id:1,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
+    for i in 0..copynumber{
+        l0.add.push(GateAdd{
+            i_ids:[2*i],
+            o_id:2*i+1,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+        l0.add.push(GateAdd{
+            i_ids:[2 * i + 1],
+            o_id:2 * i + 1,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+
+        l0.add.push(GateAdd{
+            i_ids:[2*i+1],
+            o_id:2*i,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+        
+    }
+    
+    std::iter::repeat(()).take(layernumber).for_each(|_| {
+        circuit.layers.push(l0.clone());
     });
-
-    l1.add.push(GateAdd{
-        i_ids:[1],
-        o_id:1,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
-    });
-
-    l1.add.push(GateAdd{
-        i_ids:[1],
-        o_id:0,
-        coef:halo2curves::bn256::Fr::from(1 as u32),
-        is_random:false,
-        gate_type:1,
-    });
-
-    circuit.layers.push(l1.clone());
-
-
-
-
+  
     circuit
+   
 }
-
 
 fn main(){
 
-   //choose circuit to try
-   //let mut circuit=gen_simple_circuit::<BN254Config>();
-   let mut circuit=gen_one_layer_fibonacci_circuit::<BN254Config>();
+   
+   let mut circuit=build_fibonacci(2, 20);
 
 
    println!("circuit generated as \n {:?}",circuit);
