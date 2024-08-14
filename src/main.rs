0a8794e6e3af490486e50ee965e1b169617aa2b5
ShuangWu121 <shuang.wu@posteo.net>
2024-08-14 15:40:06 +0200

    add parallel fibonaci circuit
    Signed-off-by: ShuangWu121 <shuang.wu@posteo.net>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 
 use expander_rs::{
-    circuit, BN254Config, Circuit, CircuitLayer, Config, GKRConfig, GKRScheme, GateAdd, GateMul, GateUni, M31ExtConfig, Prover, Verifier
+    circuit, BN254Config, Circuit, CircuitLayer, Config, GKRConfig, GKRScheme, GateAdd, GateMul, GateUni, M31ExtConfig, Prover, Verifier,
 };
 use halo2curves::bn256::{self, Fr};
 use sha2::Digest;
@@ -75,10 +75,112 @@ fn build_fibonacci(layernumber: usize,copynumber: usize)->Circuit<BN254Config>{
    
 }
 
+fn build_parallel_fibonacci(roundsnumber: usize)->Circuit<BN254Config>{
+    let mut circuit=Circuit::default();
+    let mut l0=CircuitLayer::<BN254Config>::default();
+    
+    l0.input_var_num=((2 * roundsnumber) as f64).log2().ceil() as usize;
+    println!("input_var_num {:?}",l0.input_var_num);
+    l0.output_var_num=((3 * (roundsnumber-1)) as f64).log2().ceil() as usize;
+
+    //define layer 1
+
+    for i in 0..(roundsnumber-1){
+        //gate =x[i]+y[i]
+        l0.add.push(GateAdd{
+            i_ids:[2*i],
+            o_id:3*i,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+        l0.add.push(GateAdd{
+            i_ids:[2 * i + 1],
+            o_id:3 * i,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+
+        //gate x[i+1]=y[i]
+        l0.add.push(GateAdd{
+            i_ids:[2*i+1],// y[i]
+            o_id:3*i+1,
+            coef:halo2curves::bn256::Fr::from(1 as u32).neg(),
+            is_random:false,
+            gate_type:1,
+        });
+
+        
+        l0.add.push(GateAdd{
+            i_ids:[2*i+2],// x[i+1]
+            o_id:3*i+1,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+     
+        
+
+        //gate y[i+1]
+        
+        l0.add.push(GateAdd{
+            i_ids:[2*i+3],
+            o_id:3*i+2,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+        
+        
+    
+    }
+    circuit.layers.push(l0.clone());
+
+    let mut l1=CircuitLayer::<BN254Config>::default();
+
+    l1.input_var_num=((3 * (roundsnumber-1)) as f64).log2().ceil() as usize;
+    l1.output_var_num=((2 * roundsnumber) as f64).log2().ceil() as usize;
+    
+    for i in 0..(roundsnumber-1){
+        //y[i+1]=x[i]+y[i]
+        l1.add.push(GateAdd{
+            i_ids:[3 * i ],
+            o_id:2* i,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+    
+        //gate x[i+1]=y[i]
+        l1.add.push(GateAdd{
+            i_ids:[3*i+2],// y[i]
+            o_id:2*i,
+            coef:halo2curves::bn256::Fr::from(1 as u32).neg(),
+            is_random:false,
+            gate_type:1,
+        });
+
+        l1.add.push(GateAdd{
+            i_ids:[3*i+1],// y[i]=x[i]
+            o_id:2*i+1,
+            coef:halo2curves::bn256::Fr::from(1 as u32),
+            is_random:false,
+            gate_type:1,
+        });
+    
+    }
+
+    circuit.layers.push(l1.clone());
+    circuit
+   
+}
+
 fn main(){
 
    //sometime, the same parameter that run successfully can cause overflow problem with bad proof, donnot know why
-   let mut circuit=build_fibonacci(2, 2);
+   //let mut circuit=build_fibonacci(2, 2);
+   let mut circuit=build_parallel_fibonacci(2);
 
 
    println!("circuit generated as \n {:?}",circuit);
@@ -87,15 +189,38 @@ fn main(){
 
    //circuit.set_random_input_for_test();
 
+   //circuit.layers[0].input_vals.evals = (0..(1 << circuit.log_input_size()))
+     //       .map(|_| <BN254Config as GKRConfig>::SimdCircuitField::one())
+       //     .collect();
+
+   
+
+   let mut x=<BN254Config as GKRConfig>::SimdCircuitField::one();
+   let mut y=<BN254Config as GKRConfig>::SimdCircuitField::one();
+   
    circuit.layers[0].input_vals.evals = (0..(1 << circuit.log_input_size()))
-            .map(|_| <BN254Config as GKRConfig>::SimdCircuitField::one())
-            .collect();
+   .flat_map(|_| {
+       let x_next = y;
+       let y_next = x + y;
+
+       let result = vec![x, x_next]; // Collect (x, y) values for the current round
+
+       // Update x and y for the next iteration
+       x = x_next;
+       y = y_next;
+
+       result
+   })
+   .take(1 << circuit.log_input_size()) // Ensure the output length matches the required size
+   .collect();
+   
+
    println!("inputs of the circuits \n: {:?}",
             circuit.layers[0]
                 .input_vals
                 .evals
                 .iter()
-                //.take(2)
+                .take(2)
                 .collect::<Vec<_>>());
    println!("start evaluating circuit \n");
    circuit.evaluate();
@@ -103,14 +228,14 @@ fn main(){
    println!("circuit evaluated as \n {:?}",circuit.layers);
 
    println!("Output of the circuit: {:?}",
-   circuit.layers
-       .last()
-       .unwrap()
-       .output_vals
-       .evals
-       .iter()
-       //.take(10)
-       .collect::<Vec<_>>());
+     circuit.layers
+            .last()
+            .unwrap()
+            .output_vals
+            .evals
+            .iter()
+            .take(10)
+            .collect::<Vec<_>>());
 
    let mut prover=Prover::new(&Config::<BN254Config>::new(GKRScheme::Vanilla));
    prover.prepare_mem(&circuit);
@@ -134,11 +259,11 @@ fn main(){
         .for_each(|b| print!("{} ", b));
    println!();
 
-   println!("Proof hash: ");
-    sha2::Sha256::digest(&proof.bytes)
-        .iter()
-        .for_each(|b| print!("{} ", b));
-    println!();
+   //println!("Proof hash: ");
+    //sha2::Sha256::digest(&proof.bytes)
+    //    .iter()
+    //    .for_each(|b| print!("{} ", b));
+    //println!();
 
     // Verify
     let verifier = Verifier::new(&Config::<BN254Config>::new(GKRScheme::Vanilla));
